b7e93e1ea8d8d3e2a75f3a4e18a528642cce036a
Alfredo <21030761@itcelaya.edu.mx>
2025-04-21 22:13:30 -0600

    States matrix to analize identifier created

--- data/TransitionMatrixes/identifier_matrix.py
+++ data/TransitionMatrixes/identifier_matrix.py
@@ -1,8 +1,32 @@
-letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-digitos = "0123456789"
+# data/TransitionMatrixes/identifier_matrix.py
+from enum import IntEnum
+import data.dictionary as dictionary
+
+class IdentifierStates(IntEnum):
+    INI_STATE      = 1
+    PRE_BODY_STATE = 2
+    BODY_STATE     = 3
+    END_STATE      = 4
+
+chars   = dictionary.dictionary['chars']
+numbers  = dictionary.dictionary['numbers']
+spaces  = dictionary.dictionary['spaces']
 
 identifier_matrix = {
-    0: {'@': 1},  # El estado 0 solo acepta el símbolo '@'
-    1: {c: 2 for c in letras},  # El estado 1 acepta letras, y pasa al estado 2
-    2: {c: 2 for c in letras + digitos + '_'},  # El estado 2 acepta letras, números o guion bajo, se queda en 2
+    IdentifierStates.INI_STATE: {
+        '@': IdentifierStates.PRE_BODY_STATE
+    },
+    IdentifierStates.PRE_BODY_STATE: {
+        **{char: IdentifierStates.BODY_STATE for char in chars},
+        '_': IdentifierStates.BODY_STATE ,
+        **{space: IdentifierStates.END_STATE for space in spaces},
+        '.': IdentifierStates.END_STATE,
+    },
+    IdentifierStates.BODY_STATE: {
+        **{char: IdentifierStates.BODY_STATE for char in chars},
+        **{number: IdentifierStates.BODY_STATE for number in numbers},
+        '_': IdentifierStates.BODY_STATE ,
+        **{space: IdentifierStates.END_STATE for space in spaces},
+        '.': IdentifierStates.END_STATE,
+    },
 }
